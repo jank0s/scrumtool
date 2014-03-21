b4e67ae74817bd851a8910cf110ac85d27be5bd9
jan@k0s.si <jan@k0s.si>
2014-03-21 08:37:24 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tpo11::Application.config.secret_key_base = '2a1fa9b9bdbeafee0a608291fcdff4c4b2fee10e9093894632223db341315837982c7fc582b64040f658b408181cd2f3b62817426bea4030752f64a2d65d1225'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tpo11::Application.config.session_store :cookie_store, key: '_tpo11_session'

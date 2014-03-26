cead2ce07fe59e1c2eec6df9888c2977d2150d71
jan@k0s.si <jan@k0s.si>
2014-03-26 15:15:16 +0100

    Reformat /models/user.rb code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,33 @@
 class User < ActiveRecord::Base
-  belongs_to :role
-	before_create :create_remember_token
+    belongs_to :role
+    before_create :create_remember_token
 
-  has_secure_password
+    has_secure_password
 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :username, presence:   true, length: { minimum: 3 }, uniqueness: true
-  validates :name, presence:   true
-  validates :surname, presence:   true
-	validates :email, presence:   true, format: { with: VALID_EMAIL_REGEX }
-  validates :role_id, presence:   true
-	validates :password, length: { minimum: 3 }, :if => :validate_password?
-  validates :password_confirmation, presence: true, :if => :validate_password?
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :username, presence: true, length: {minimum: 3}, uniqueness: true
+    validates :name, presence: true
+    validates :surname, presence: true
+    validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+    validates :role_id, presence: true
+    validates :password, length: {minimum: 3}, :if => :validate_password?
+    validates :password_confirmation, presence: true, :if => :validate_password?
 
-	def User.new_remember_token
-    	SecureRandom.urlsafe_base64
-  end
+    def User.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
 
-  def User.encrypt(token)
-    	Digest::SHA1.hexdigest(token.to_s)
-  end
+    def User.encrypt(token)
+        Digest::SHA1.hexdigest(token.to_s)
+    end
 
-  def validate_password?
-    password.present? || password_confirmation.present?
-  end
+    def validate_password?
+        password.present? || password_confirmation.present?
+    end
 
-  private
-    	def create_remember_token
-        	self.remember_token = User.encrypt(User.new_remember_token)
-      end
+    private
+    def create_remember_token
+        self.remember_token = User.encrypt(User.new_remember_token)
+    end
 
 end

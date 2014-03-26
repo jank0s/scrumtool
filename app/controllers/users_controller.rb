6304b1f6c7d63c4db84118b12e3038402a6b6f6a
jan@k0s.si <jan@k0s.si>
2014-03-26 13:39:13 +0100

    Updated edit view and user_params so admin can edit role id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,61 +1,62 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:index, :show, :new, :create]
-	before_action :correct_user, only: [:show, :edit, :update]
-	before_action :admin_user, only: [:index, :new, :create]
+    before_action :signed_in_user, only: [:index, :show, :new, :create]
+    before_action :correct_user, only: [:show, :edit, :update]
+    before_action :admin_user, only: [:index, :new, :create]
 
-	def index
-		@users=User.all
-	end
-
-	def show
-
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params_user)
-      redirect_to users_url
-    else
-      render 'edit'
-    end
-  end
-
-	def new
-		@user = User.new
-	end
-
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			redirect_to users_url
-		else
-			render "new"
-		end
-	end
-
-	private
-		def user_params
-			params.require(:user).permit(:username, :password, :password_confirmation, :email, :name, :surname, :role_id)
+    def index
+        @users=User.all
     end
 
-    def user_params_user
-      params.require(:user).permit(:username, :password, :password_confirmation, :email, :name, :surname)
+    def show
+
     end
 
-		def signed_in_user
-			redirect_to signin_url unless signed_in?
-		end
+    def edit
+        @user = User.find(params[:id])
+    end
 
-		def correct_user
-			@user = User.find(params[:id])
-			redirect_to root_url unless admin? || current_user?(@user)
-		end
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            redirect_to users_url
+        else
+            render 'edit'
+        end
+    end
 
-		def admin_user
-			redirect_to root_url unless admin?
-		end
-	end
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to users_url
+        else
+            render "new"
+        end
+    end
+
+    private
+    def user_params
+        if admin?
+            params.require(:user).permit(:username, :password, :password_confirmation, :email, :name, :surname, :role_id)
+        else
+            params.require(:user).permit(:username, :password, :password_confirmation, :email, :name, :surname)
+        end
+
+    end
+
+    def signed_in_user
+        redirect_to signin_url unless signed_in?
+    end
+
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to root_url unless admin? || current_user?(@user)
+    end
+
+    def admin_user
+        redirect_to root_url unless admin?
+    end
+end

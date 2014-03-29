59c2d28efb13539147bce8e5a2a194f4b49ddfe3
Maticcc <matic.fri@gmail.com>
2014-03-29 23:43:53 +0100

    Sprint

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -7,5 +7,19 @@ class SprintsController < ApplicationController
   def new
     @sprint = Sprint.new
   end
-  
+
+  def create
+    @sprint = Sprint.new(sprint_params)
+
+    if @sprint.save
+      redirect_to sprints_url
+    else
+      render "new"
+    end
+  end
+
+  private
+  def sprint_params
+    params.require(:sprint).permit(:number, :start, :end, :velocity)
+  end
 end

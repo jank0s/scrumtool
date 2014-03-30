81d380228788691bd0a9b0b52ad7b0fc4b8c74e6
Maticcc <matic.fri@gmail.com>
2014-03-30 23:20:27 +0200

    Sprint validation start

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,4 +1,18 @@
 class Sprint < ActiveRecord::Base
 
+  validates :start, presence: true
+  validates :end, presence: true
   validates :velocity, presence: true, numericality: {only_integer: true}
+
+  validate :raketa
+
+  private
+  def raketa
+    if self[:start] < self[:end]
+      return true
+    else
+      errors.add(:start, "must be before end")
+    end
+  end
+
 end

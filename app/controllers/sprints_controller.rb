fd42a9225786caecdf95297ae11c186ca3381f3a
Maticcc <matic.fri@gmail.com>
2014-03-31 13:12:30 +0200

    Sprint number self increment

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -9,13 +9,28 @@ class SprintsController < ApplicationController
   end
 
   def create
+    lastSprint = Sprint.last
+
+    if lastSprint == nil
+      num = 1
+    else
+      num = lastSprint.number
+      num += 1
+    end
+
     @sprint = Sprint.new(sprint_params)
+    @sprint.number = num
 
     if @sprint.save
       redirect_to sprints_url
     else
       render "new"
     end
+  end
+
+  def destroy
+    Sprint.find(params[:id]).destroy
+    redirect_to sprints_url
   end
 
   private

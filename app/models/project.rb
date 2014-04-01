8f4dccaf3d10e29c37f776bebacf58adfb9e190a
jan@k0s.si <jan@k0s.si>
2014-04-01 10:11:05 +0200

    Edit users updated with teammembers

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 	belongs_to :scrummaster, :class_name => 'User'
 	belongs_to :productowner, :class_name => 'User'
-	has_many :teammembers
+	has_many :teammembers, :dependent => :destroy
 	has_many :users, through: :teammembers
   has_many :sprints
     validates :name, presence: true, length: {minimum: 3, maximum: 6}, uniqueness: true

5a4d127743c1241be49531b3e53b6f1a3ec2c728
jan@k0s.si <jan@k0s.si>
2014-04-06 02:03:01 +0200

    New story controller and view

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,8 +1,40 @@
 class StoriesController < ApplicationController
+    before_action :signed_in_user
+    before_action :correct_user
+    before_action :po_or_sm, only: [:new, :create]
+
     def index
     end
 
     def new
         @story = Story.new
+    end
+
+    def create
+        @story = Story.new(story_params)
+        @story.project=current_user.activeproject
+        @story.finished=false
+        if @story.save
+            redirect_to stories_url
+        else
+            render "new"
+        end
+    end
+
+    private
+    def story_params
+        params.require(:story).permit(:name, :description, :test, :priority_id, :value)
+    end
+
+    def signed_in_user
+        redirect_to signin_url unless signed_in?
+    end
+
+    def correct_user
+        redirect_to root_url unless current_user.activeproject!=nil
+    end
+
+    def po_or_sm
+        redirect_to root_url unless (productowner? || scrummaster? || admin?)
     end
 end

5f2529957e538f51a54ca67741ec49f306ff642a
jan@k0s.si <jan@k0s.si>
2014-04-06 02:05:31 +0200

    Story basic validation

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,11 @@
 class Story < ActiveRecord::Base
     belongs_to :project
     belongs_to :priority
+
+    validates :project_id, :presence => true
+    validates :name, :presence => true
+    validates :description, :presence => true
+    validates :priority_id, :presence => true
+    validates :value, :presence => true
+    validates :finished, :presence => true
 end

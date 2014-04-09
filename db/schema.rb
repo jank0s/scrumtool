4c0af3f9a39ab9c322f9231871bdd4f47e6a9ab5
Maticcc <matic.fri@gmail.com>
2014-04-09 21:54:25 +0200

    Task list...

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140409182242) do
-
+ActiveRecord::Schema.define(version: 20140409191148) do
 
   create_table "priorities", force: true do |t|
     t.string   "name"

e39ac961bc7dbe76c72c3c89ed8685878c7062eb
mojcak <mojca.komavec@gmail.com>
2014-04-09 21:17:26 +0200

    Added timeestimates in story model, timeestimates button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409173507) do
+ActiveRecord::Schema.define(version: 20140409191148) do
 
   create_table "priorities", force: true do |t|
     t.string   "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140409173507) do
     t.boolean  "finished"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "timeestimates"
   end
 
   create_table "tasks", force: true do |t|

2b8fa18ac1780f10ee899410b407d25dcffbaf4a
jan@k0s.si <jan@k0s.si>
2014-04-20 23:37:39 +0200

    Show accept only if story is finished

--- app/models/story.rb
+++ app/models/story.rb
@@ -11,6 +11,19 @@ class Story < ActiveRecord::Base
     validates :description, :presence => true, length: {minimum: 3}
     validates :priority_id, :presence => true
     validates :value, :numericality => {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}, :allow_blank => true
+    #validate :validate_finished
     #validates :timeestimates, :numericality => {:greater_than_or_equal_to => 0.0, :less_than_or_equal_to => 100.0}
+
+    def done?
+        @t=tasks
+        !@t.empty? && @t.where(completed: false).empty?
+    end
+
+    private
+    def validate_finished
+        if self[:finished]=true && !self.done?
+            errors[:base] << "Story is not completed"
+        end
+    end
 
 end

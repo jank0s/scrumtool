e96dff17cd1a0e07ae20bb7bf9f234590bf385c5
Edin <edinmahmutovic5@gmail.com>
2014-04-20 02:47:36 +0200

    table posts update

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417144214) do
+ActiveRecord::Schema.define(version: 20140418035110) do
 
   create_table "posts", force: true do |t|
     t.text     "value"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
   create_table "priorities", force: true do |t|

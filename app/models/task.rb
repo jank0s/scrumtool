3e5dd5b3fe9775b3556cdaac5c70616b01c4c218
Maticcc <matic.fri@gmail.com>
2014-04-22 09:46:04 +0200

    Fix

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,6 @@ class Task < ActiveRecord::Base
 	has_many :worktimes
 	
 	validates :name, :presence => true, :uniqueness => {:case_sensitive => false}
-	validates :time_estimation, :presence => true, :numericality => {:greater_than_or_equal_to => 1, :less_than_or_equal_to => 33}, :allow_blank => true
+	validates :time_estimation, :presence => true, :numericality => {:greater_than_or_equal_to => 1, :less_than_or_equal_to => 12}, :allow_blank => true
 
 end

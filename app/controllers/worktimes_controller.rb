cd6eff2d45e3749680c75f0c6cb12687e1e76040
Maticcc <matic.fri@gmail.com>
2014-05-31 23:55:29 +0200

    Fix

--- app/controllers/worktimes_controller.rb
+++ app/controllers/worktimes_controller.rb
@@ -22,23 +22,46 @@ class WorktimesController < ApplicationController
     @time_id = params[:time_id]
     @time_done = params[:time_done]
     @time_remaining = params[:time_remaining]
+    @time_day = params[:time_day]
 
 
     @t = Task.find(params[:task_id])
 
-    if @time_remaining.last.to_f == 0.0
-      @t.update_attributes(completed: true)
-    else
+    #if @time_remaining.last.to_f == 0.0
+    #  @t.update_attributes(completed: true)
+    #else
+    #  @t.update_attributes(completed: false)
+    #end
+    @lst = []
+    today = Date.today
+    i = 0
+    j = 0
+    inside = false
+    @time_id.each do |time|
+      @time = Worktime.find(time)
+      if @time.day == today && @time_remaining[i].to_f == 0.0
+        @lst << @time.day
+        @lst << @time_remaining[i].to_f
+        @t.update_attributes(completed: true)
+        inside = true
+      end
+      if @time.day > today
+        i-=1
+        @time.update_attributes(done: @time_done[j].to_f, remaining: @time_remaining[i].to_f)
+        j+=1
+        i+=1
+      else
+        @time.update_attributes(done: @time_done[j].to_f, remaining: @time_remaining[i].to_f)
+        i+=1
+        j+=1
+      end
+    end
+
+    if !inside
       @t.update_attributes(completed: false)
     end
 
-    i = 0
-    @time_id.each do |time|
-      @time = Worktime.find(time)
 
-      @time.update_attributes(done: @time_done[i].to_f, remaining: @time_remaining[i].to_f)
-      i+=1
-    end
     #TUUUUUUUUUUUUKI DA SE VID SAMO ODKAR SI ASSIGNAN
     @id = Worktime.find(@time_id.first).task_id
     @worktimes = Worktime.where(task_id: @id).order(:day)

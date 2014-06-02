b835aad6424447f695d5f4f1520000a0a7ebbe92
Maticcc <matic.fri@gmail.com>
2014-06-02 13:08:59 +0200

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,15 +21,31 @@ Priority.create(name: "Won't have this time")
 Project.create(name: "Scrumer", description: "Something", scrummaster_id: 2, user_ids: [1,2,3,4,5,6])
 Sprint.create(start: "2014-05-20", end: "2014-05-24", velocity: 3, project_id: 1)
 Sprint.create(start: "2014-05-28", end: "2014-06-03", velocity: 3, project_id: 1)
-Story.create(project_id: 1, name: "Story1", description: "Story one", priority_id: 1, sprint_id: 2, finished: false, test: "", timeestimates: 2)
-Story.create(project_id: 1, name: "Story2", description: "Story two", priority_id: 1, sprint_id: 2, finished: false, test: "", timeestimates: 1)
-Story.create(project_id: 1, name: "Story3", description: "Story three", priority_id: 1, finished: false, test: "", timeestimates: 1)
-Story.create(project_id: 1, name: "Story4", description: "Story four", priority_id: 1, finished: true, test: "", timeestimates: 3, finished_in_sprint: 1)
 
-Task.create(name: "Task1.2", story_id: "1", completed: false, time_estimation: 6, assigned_date: "2014-05-28 00:00:00", assigned_to: 2)
-Task.create(name: "Task2.2", story_id: "1", completed: false, time_estimation: 2, assigned_date: "2014-06-02 00:00:00")
-Task.create(name: "Task3.2", story_id: "1", completed: false, time_estimation: 4, assigned_date: "2014-05-28 00:00:00", assigned_to: 2)
-Task.create(name: "Task4.2", story_id: "1", completed: false, time_estimation: 5, assigned_date: "2014-06-02 00:00:00")
+
+#Story.create(project_id: 1, name: "Story1", description: "Story one", priority_id: 1, sprint_id: 2, finished: false, test: "", timeestimates: 2)
+#Story.create(project_id: 1, name: "Story2", description: "Story two", priority_id: 1, sprint_id: 2, finished: false, test: "", timeestimates: 1)
+#Story.create(project_id: 1, name: "Story3", description: "Story three", priority_id: 1, finished: false, test: "", timeestimates: 1)
+#Story.create(project_id: 1, name: "Story4", description: "Story four", priority_id: 1, finished: true, test: "", timeestimates: 3, finished_in_sprint: 1)
+
+Story.create(project_id: 1, name: "Dodajanje projekta", description: "Administrator lahko ustvari nov projekt, izbere uporabnike za delo na tem projektu in določi njihove projektne vloge (produktni vodja, skrbnik metodologije, član razvojne skupine).", priority_id: 1, sprint_id: 2, finished: false, test: "", timeestimates: 2, test: "# Preveri dodajanje novega projekta in morebitno podvajanje imen projektov.
+# Preveri izbiro uporabnikov za delo na projektu.
+# Preveri določitev projektnih vlog.")
+Story.create(project_id: 1, name: "Ustvarjanje novega Sprinta", description: "Skrbnik metodologije lahko ustvari nov Sprint. Določi mu začetni in končni datum ter pričakovano hitrost.", priority_id: 1, sprint_id: 2, finished: false, test: "", timeestimates: 1, test: "# Preveri običajen potek: dodaj nov Sprint, določi mu začetni in končni datum (v prihodnosti) in nastavi začetno hitrost.
+# Preveri za primer, ko je končni datum pred začetnim.
+# Preveri za primer, ko je začetni datum v preteklosti.
+# Preveri za neregularno vrednost hitrosti Sprinta.
+# Preveri za primer, ko se dodani Sprint prekriva s katerim od obstoječih.")
+Story.create(project_id: 1, name: "Dodajanje opomb k zgodbam", description: "Člani razvojne skupine lahko k uporabniškim zgodbam v dogovoru s produktnim vodjo ali skrbnikom metodologije dopisujejo svoje opombe.", priority_id: 1, finished: false, test: "", timeestimates: 1, test: "# Preveri regularen potek.")
+Story.create(project_id: 1, name: "Ocena časovne zahtevnosti", description: "Skrbnik metodologije lahko nedodeljeni zgodbi v seznamu zahtev določi (oziroma spremeni) oceno časovne zahtevnosti.", priority_id: 1, finished: true, test: "", timeestimates: 3, finished_in_sprint: 1, test: "# Preveri regularen potek.
+# Preveri veljavnost ocene časovne zahtevnosti.
+# Preveri za zgodbo, ki je že dodeljena nekemu Sprintu.")
+
+
+Task.create(name: "Logika", story_id: "1", completed: false, time_estimation: 6, assigned_date: "2014-05-28 00:00:00", assigned_to: 2)
+Task.create(name: "Uporabniški vmesnik", story_id: "1", completed: false, time_estimation: 2, assigned_date: "2014-06-02 00:00:00")
+Task.create(name: "Baza", story_id: "1", completed: false, time_estimation: 4, assigned_date: "2014-05-28 00:00:00", assigned_to: 2)
+Task.create(name: "Testiranje", story_id: "1", completed: false, time_estimation: 5, assigned_date: "2014-06-02 00:00:00")
 
 #Task.create(name: "Task1.1", story_id: "1", completed: false, time_estimation: 4, assigned_date: "2014-05-28 00:00:00")
 #Task.create(name: "Task2.1", story_id: "1", completed: false, time_estimation: 6, assigned_date: "2014-05-28 00:00:00")
